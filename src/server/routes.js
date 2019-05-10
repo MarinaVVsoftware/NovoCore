5d94eb06f2d052c1d1730ff6e20fb76bc973b598
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-05-10 11:20:46 -0500

    Endpoints marina

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,12 +2,14 @@
 // de routes, aquí se carga el arbol superior de las rutas y facilita
 // entender el ruteo de la API. También ayuda a el paso de parámetros de instancias
 // necesarias.
-const Users = require("../routes/Users");
-const Log = require("../helpers/Logs");
+const Users = require('../routes/Users');
+const Marina = require('../routes/Marina');
+const Log = require('../helpers/Logs');
 
 module.exports = (app, router, mysqlConnection) => {
-  /* Rutas de login */
-  Users(app, router, mysqlConnection);
+	/* Rutas de login */
+	Users(app, router, mysqlConnection);
+	Marina(app, router, mysqlConnection);
 
-  Log.Success("Rutas de la API cargadas.");
+	Log.Success('Rutas de la API cargadas.');
 };
