5e6d532be77481864d0b7c4d025e17a1d59bcc4e
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-06-03 14:12:03 -0500

    Comments

--- src/routes/marina/Marina.js
+++ src/routes/marina/Marina.js
@@ -5,11 +5,15 @@ const { MarinaSchema } = require("../../schemas/MarinaSchema");
 module.exports = (app, router, newError, Query, validate, mysqlConnection) => {
 	const instances = [ newError, Query, mysqlConnection ];
 
+	// Lee una cotización por ID.
 	router.get("/api/marina/quotations/:id", validate({ params: MarinaSchema.read }), Marina.Read(...instances));
+	// Lee todas las cotizaciones por grupo.
 	router.get("/api/marina/quotations/", validate({ query: MarinaSchema.readList }), Marina.ReadList(...instances));
+	// Crea cotización.
+	router.post("/api/marina/quotations/", validate({ body: MarinaSchema.create }), Marina.Create(...instances));
+
 	router.delete("/api/marina/quotations/erase", validate({ body: MarinaSchema.erase }), Marina.Erase(...instances));
 	router.patch("/api/marina/quotations/delete", validate({ body: MarinaSchema.delete }), Marina.Delete(...instances));
-	router.post("/api/marina/quotations/create", validate({ body: MarinaSchema.create }), Marina.Create(...instances));
 	router.put("/api/marina/quotations/update", validate({ body: MarinaSchema.update }), Marina.Update(...instances));
 
 	app.use(router);
