40c88f405fdc19345636d7bda726baf49e36d088
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-06-12 16:57:31 -0500

    Cambios en alpha de algoritmos 3

--- src/helpers/datetime/FormatDatetime.js
+++ src/helpers/datetime/FormatDatetime.js
@@ -1,25 +1,42 @@
 /** Convierte una fecha en formato legible para el humano
  */
 
-class FormatDatetime {
-  HumanFormat(date) {
-    return (
-      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
-    );
-  }
+const FormatDatetime = {};
 
-  GetMonthBetweenTwoDates(a, b) {
-    let year1 = a.getFullYear();
-    let year2 = b.getFullYear();
-    let month1 = a.getMonth();
-    let month2 = b.getMonth();
-    if (month1 === 0) {
-      month1++;
-      month2++;
-    }
-    let months = (year2 - year1) * 12 + (month2 - month1) - 1;
-    return months + 1;
+FormatDatetime.HumanFormat = function(date) {
+  return (
+    date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+  );
+};
+
+FormatDatetime.GetMonthBetweenTwoDates = function(a, b) {
+  let year1 = a.getFullYear();
+  let year2 = b.getFullYear();
+  let month1 = a.getMonth();
+  let month2 = b.getMonth();
+  if (month1 === 0) {
+    month1++;
+    month2++;
   }
-}
+  let months = (year2 - year1) * 12 + (month2 - month1) - 1;
+  return months + 1;
+};
+
+/** Calcula los días entre una fecha y otra.
+ * @param {Date} startDate startDate
+ * @param {Date} endDate endDate
+ */
+FormatDatetime.TimeBetweenTwoDates = function(startDate, endDate) {
+  const A = new Date(startDate);
+  const B = new Date(endDate);
+
+  if (A > B) {
+    let days = (A.getTime() - B.getTime()) / 1000 / 60 / 60 / 24;
+    return days + 1;
+  } else {
+    let days = (B.getTime() - A.getTime()) / 1000 / 60 / 60 / 24;
+    return days + 1;
+  }
+};
 
 module.exports = FormatDatetime;
--- src/helpers/occupationAlgorithms/OccupationAvailability.js
+++ src/helpers/occupationAlgorithms/OccupationAvailability.js
@@ -13,7 +13,9 @@ module.exports = function OccupationAvailability(
   loa,
   quotationId
 ) {
-  let occupation = [];
+  let fragments = [];
+  let longerFragment = null;
+  let slipValid = null;
   let slipType = null;
   let error = null;
 
@@ -33,7 +35,7 @@ module.exports = function OccupationAvailability(
   if (!slipType) {
     error =
       "El tamaño del barco no corresponde con los tipos de slips disponibles. Rangos entre 1ft y 120ft.";
-    return { occupation, error };
+    return { slipValid, error };
   }
 
   /* Reduce la cantidad de slips en el array a solo los del tipo de slip */
@@ -41,57 +43,153 @@ module.exports = function OccupationAvailability(
     if (slip.slip_type_id == slipType) return slip;
   });
 
-  /* Reduce la cantidad de ocupaciones en el array a solo los del tipo de slip */
+  var slipsReduced = slipsReduced.slice(0, 7);
+
+  /* Reduce la cantidad de ocupaciones en el array a solo los del tipo de slip*/
   var slipsOccupationReduced = slipsOccupation.filter(slipOccupation => {
     if (slipOccupation.slip_type_id == slipType) return slipOccupation;
   });
 
-  // "disponibilidad entera": slip que posea la mayor ocupación para las fechas de esa cotización
-  /* busca el slip mas cercano disponible, que cumpla con la mejor "disponibilidad entera" */
-  for (let s = 0; s < slipsReduced.length; s++) {
-    // hace un filter de todas las ocupaciones con ese slip.
-    if (slipsOccupationReduced.length == 0) {
-      console.log("te quedaste sin ocupaciones para revisar!");
-      break;
-    } else
-      console.log("ocupaciones por revisar: " + slipsOccupationReduced.length);
-    var occupationBySlip = slipsOccupationReduced.filter((occup, index) => {
-      if (slipsReduced[s].slip_name == occup.slip_name) {
-        slipsOccupationReduced.splice(index, 1);
-        return occup;
+  /* busca el slip mas cercano disponible, que cumpla con la mejor "disponibilidad entera".
+  Si no hay ocupaciones después de generar los filtros, lo inserta en el primer 
+  slip disponible, de lo contrario tendrá que buscar alguno disponible. */
+  if (slipsOccupationReduced.length > 0)
+    /* Itera cada slip posible en el que el barco puede quedarse */
+    for (let s = 0; s < slipsReduced.length; s++) {
+      /* Obtiene todas las ocupaciones de ese slip */
+      var occupationBySlip = slipsOccupationReduced.filter(occup => {
+        if (slipsReduced[s].slip_id == occup.slip_id) return occup;
+      });
+
+      if (occupationBySlip.length > 0) {
+        // create all fragments
+      } else {
+        slipValid = slipsReduced[s].slip_name;
+        break;
       }
-    });
-    // si encuentra cero ocupaciones, entonces lo setea directamente
-    if (occupationBySlip.length > 0) console.log(occupationBySlip);
-
-    // si retorna resultados, filtra todas las ocupaciones de ese slip por coincidencia de fecha
-    // si no hay ocupaciones entre las fechas de estadía, entonces lo setea directamente
-
-    // si se cumple que hay coincidencia tanto en el slip como en fechas, calcula la mejor disponibilidad.
-    // dado que no hubo disponibilidad entera, pasa al siguiente slip.
+    }
+  else {
+    slipValid = slipsReduced[0].slip_name;
   }
 
-  return { slipsReduced, slipsOccupationReduced };
+  if (slipValid === null)
+    error = "No se encontró ninguna disponibilidad entera.";
 
-  // /* algoritmo para cálculo de disponibilidad de estadía */
-
-  // // recibe un rango de fechas de estadía, el tamaño del barco, el número de la cotización
-  // // "disponibilidad entera": slips que posea la mayor ocupación para el barco
-
-  // // busca el slip mas cercano disponible
-  //   // guarda la cantidad de dias disponibles totales válidos para las fechas de estadía dadas
-  //   // compara con todos los slips buscando alguno que cumpla con una mejor "disponibilidad entera"
-
-  //   // si no encuentra ninguno, se queda con la disponibilidad inicial obtenida.
-  //   // si no encuentra espacio en ningún slip, ni de un dia, retorna indisponibilidad.
-
-  //   // si quedan días de ocupación por asignar, repite el proceso.
-
-  // // cada vuelta equivale a un row dentro de la tabla, dado que se hará en slips diferentes la ocupación.
-  // // en ese caso se considera que el barco estará en varios slips diferentes durante su estadía.
-
-  // // si un barco grande no encuentra ocupación suficiente en los slips de su tamaño, manda a llamar al
-  // // algoritmo "multisplips" para calcular ocupación ocupando varios slips al mismo tiempo.
-
-  //   // si falla, retorna indisponibilidad.
+  return {
+    slipValid,
+    error,
+    fragments,
+    slipsReduced,
+    slipsOccupationReduced
+  };
 };
+
+/**
+ *
+ * @param {Date} S QuotationStartDate
+ * @param {Date} E QuotationEndDate
+ * @param {array} occupationsBySlip arreglo de ocupaciones del slip
+ * @param {string} slip nombre del slip que se está iterando
+ * @param {array} fragments contenedor de intervalos de tiempo
+ */
+function CalculateFragments(S, E, occupationsBySlip, slip, fragments) {
+  let n = occupationsBySlip.length; //cantidad de ocupaciones del slip
+  let days = null;
+
+  /* tipos de intervalos */
+
+  // S <=> A : antes de la ocupación
+  // B <=> E : después de la ocupación
+
+  occupationsBySlip.forEach((occupation, index) => {
+    let A = occupation.start_date;
+    let B = occupation.end_date;
+
+    if (S < A) {
+      if (S > B) {
+        /* caso especial, donde existe un intervalo antes de una ocupación */
+        days = FormatDatetime.TimeBetweenTwoDates(S, A);
+        fragments.push({
+          slip: slip,
+          startDate: S,
+          endDate: A,
+          days: days
+        });
+      }
+    } else {
+      /* si no es menor, es porque es una ocupación cuyo startDate (A)
+      se encuentra después de S. Aquí empiezan los intervalos normales. */
+
+      /* en el último caso, dado que no habrán más ocupaciones, el último fragmento
+      usa una fórmula diferente, donde F= B, E */
+      if (index < occupation.length) {
+        // casos normales, con formula normal
+        // si no hay dias entre ocupaciones, no se crea el fragmento
+      } else {
+        // ultimo caso, con formula especial
+      }
+    }
+  });
+
+  // primero hay que encontrar el caso especial, donde existe un intervalo antes
+  // de la primera ocupación. Si startDate es menor que la 1ra ocupación, existe el intervalo.
+  if (S < occupationsBySlip[0].start_date) {
+    days = FormatDatetime.TimeBetweenTwoDates(S, endDate);
+    fragments.push({
+      slip: slip,
+      startDate: S,
+      endDate: occupationsBySlip[0].start_date,
+      days: fragment
+    });
+  }
+}
+
+/** Calcula los intervalos de tiempo en los que el barco
+ * podría quedarse porque están desocupados.
+ *
+ * @param {Date} S QuotationStartDate
+ * @param {Date} E QuotationEndDate
+ * @param {Date} A OccupationStartDate
+ * @param {Date} B OccupationEndDate
+ * @param {array} fragments contenedor de intervalos de tiempo
+ */
+function GetDateFragments(S, E, A, B, fragments, slip) {
+  // Si llega después
+  if (S >= A) {
+    if (E >= B) {
+      // y se va después (3)
+      // obtengo la dif entre E y B
+      fragments = CreateFragment(B, E, fragments, slip);
+    }
+    // else = y se va antes (2)
+  } else {
+    // si llega antes
+    // y se va despues (1)
+    if (E >= B) {
+      // y se va antes (4)
+      // obtengo la dif entre A y S
+      fragments = CreateFragment(S, A, fragments, slip);
+      // obtengo la dif entre E y B
+      fragments = CreateFragment(B, E, fragments, slip);
+    } else {
+      // obtengo la dif entre A y S
+      fragments = CreateFragment(S, A, fragments, slip);
+    }
+
+    // si llega antes y se va después no hay intervalos disponibles.
+  }
+  return fragments;
+}
+
+function CreateFragment(startDate, endDate, fragments, slip) {
+  fragment = FormatDatetime.TimeBetweenTwoDates(startDate, endDate);
+
+  fragments.push({
+    slip: slip,
+    startDate: startDate,
+    endDate: endDate,
+    days: fragment
+  });
+
+  return fragments;
+}
