2f3cbf5bae2b78aa978ae65a2bf1670cd18ef79c
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-05 16:40:27 -0500

    Hotfix -> ajuste de morgan para mostrar los logs en hora local y no UTC

--- src/helpers/morgan.js
+++ src/helpers/morgan.js
@@ -3,19 +3,22 @@ const morgan = require("morgan");
 module.exports = app => {
   // crea el token "timestamp" para morgan
   morgan.token("timestamp", () => {
-    let d = new Date();
+    let d = new Date().toLocaleString("en-US", {
+      timeZone: "America/Cancun"
+    });
+    d = new Date(d);
     let timestamp =
-      d.getUTCFullYear() +
+      d.getFullYear() +
       "/" +
-      ("0" + (d.getUTCMonth() + 1)).slice(-2) +
+      ("0" + (d.getMonth() + 1)).slice(-2) +
       "/" +
-      ("0" + d.getUTCDate()).slice(-2) +
+      ("0" + d.getDate()).slice(-2) +
       " " +
-      ("0" + d.getUTCHours()).slice(-2) +
+      ("0" + d.getHours()).slice(-2) +
       ":" +
-      ("0" + d.getUTCMinutes()).slice(-2) +
+      ("0" + d.getMinutes()).slice(-2) +
       ":" +
-      ("0" + d.getUTCSeconds()).slice(-2);
+      ("0" + d.getSeconds()).slice(-2);
 
     return timestamp;
   });
