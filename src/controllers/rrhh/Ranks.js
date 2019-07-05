17cf059ff184d2733ed0dce9542c0aaf95074b24
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-05 12:01:43 -0500

    Refactorización

--- src/controllers/rrhh/Ranks.js
+++ src/controllers/rrhh/Ranks.js
@@ -1,19 +1,16 @@
 const Ranks = {};
 
 Ranks.GetRanks = (newError, Query, mysqlConnection) => {
-  return function(req, res, next) {
+  return (req, res, next) => {
     try {
       Query(mysqlConnection, "CALL SP_Ranks_GetRanks();")
         .then(result => {
           res.status(200).send({ ranks: result[0][0] });
         })
         .catch(error => {
-          /* retorna el mensaje de error */
-          console.log(error);
-          next(error);
+          next(newError(error, 400));
         });
     } catch (error) {
-      console.log(error);
       next(newError(error, 500));
     }
   };
