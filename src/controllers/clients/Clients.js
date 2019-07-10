f826211f7cda638263c72e2ef2cd9cc25c4e59d8
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-10 15:12:24 -0500

    Adicción de BankAccounts a PostClient y fix de objetos en responses

--- src/controllers/clients/Clients.js
+++ src/controllers/clients/Clients.js
@@ -18,12 +18,10 @@ Clients.GetClientById = (newError, Query, mysqlConnection) => {
 
         Promise.all(Promises)
           .then(result => {
-            res.status(200).send({
-              client: {
-                client: result[0][0][0],
-                bankAccounts: result[1][0][0]
-              }
-            });
+            let client = result[0][0][0][0];
+            client.bankAccounts = result[1][0][0];
+
+            res.status(200).send({ client });
           })
           .catch(error => next(newError(error, 400)));
       }
@@ -48,14 +46,49 @@ Clients.GetClients = (newError, Query, mysqlConnection) => {
 Clients.PostClient = (newError, Query, mysqlConnection) => {
   return (req, res, next) => {
     try {
+      let client = req.body.client;
+      let bankAccounts = req.body.client.bankAccounts;
+      let clientId = null;
+
+      /* Crea el cliente */
       Query(mysqlConnection, "CALL SP_Clients_PostClient(?,?,?,?,?);", [
-        req.body.statusId,
-        req.body.name,
-        req.body.email,
-        req.body.phone,
-        req.body.address
+        client.statusId,
+        client.name,
+        client.email,
+        client.phone,
+        client.address
       ])
-        .then(() => res.status(201).send())
+        .then(result => {
+          /* Obtiene el id del cliente creado para usarlo en el resto de inserts */
+          clientId = result[0][0][0]["LAST_INSERT_ID()"];
+          let Promises = [];
+
+          /* Agrega a la lista de promesas cada cuenta bancaria */
+          if (bankAccounts)
+            bankAccounts.forEach(bankAccount => {
+              Promises.push(
+                Query(
+                  mysqlConnection,
+                  "CALL SP_BankAccounts_PutBankAccount(?,?,?,?,?,?);",
+                  [
+                    clientId,
+                    req.params.number,
+                    bankAccount.accountNumber,
+                    bankAccount.alias,
+                    bankAccount.bank,
+                    bankAccount.clabe
+                  ]
+                )
+              );
+            });
+
+          /* Ejecuta las promesas generadas en caso que se aniden elementos */
+          if (Promises.length > 0)
+            Promise.all(Promises)
+              .then(() => res.status(201).send())
+              .catch(error => next(newError(error, 400)));
+          else res.status(201).send();
+        })
         .catch(error => next(newError(error, 400)));
     } catch (error) {
       next(newError(error, 500));
@@ -66,16 +99,18 @@ Clients.PostClient = (newError, Query, mysqlConnection) => {
 Clients.PutClient = (newError, Query, mysqlConnection) => {
   return (req, res, next) => {
     try {
+      let client = req.body.client;
+
       if (isNaN(req.params.id))
         next(newError("el param 'id' no es un string válido.", 406));
       else
         Query(mysqlConnection, "CALL SP_Clients_PutClient(?,?,?,?,?,?);", [
           req.params.id,
-          req.body.statusId,
-          req.body.name,
-          req.body.email,
-          req.body.phone,
-          req.body.address
+          client.statusId,
+          client.name,
+          client.email,
+          client.phone,
+          client.address
         ])
           .then(() => res.status(201).send())
           .catch(error => next(newError(error, 400)));
