f214f6736ddcdb5bc35f3c5f7bfa4ecf13b7b1c2
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-11 12:25:51 -0500

    Update ElectronicWallet.js

--- src/controllers/clients/ElectronicWallet.js
+++ src/controllers/clients/ElectronicWallet.js
@@ -1,109 +1,80 @@
-const path = require("path");
-const newError = require(path.resolve(__dirname, "../../helpers/newError"));
+const ElectronicWallet = {};
 
-// Marina - Controller
-const Marina = {};
-
-// Read all the Marina Quotation
-Marina.Read = mysqlConnection => {
+ElectronicWallet.GetElectronicWallet = (newError, Query, mysqlConnection) => {
   return (req, res, next) => {
     try {
-      mysqlConnection.query(
-        "CALL SP_READ_ELECTRONIC_WALLET",
-        (err, rows, fields) => {
-          if (err) next(newError(err, 400));
-          rows.pop();
-          res.status(200).send(JSON.stringify(rows));
-        }
-      );
+      if (isNaN(req.params.id))
+        next(newError("el param 'id' no es un string válido.", 406));
+      else
+        Query(mysqlConnection, "CALL SP_ElectronicWallet_GetWallet(?);", [
+          req.params.id
+        ])
+          .then(result =>
+            res.status(200).send({ electronicWallet: result[0][0] })
+          )
+          .catch(error => next(newError(error, 400)));
     } catch (error) {
-      console.log(error);
       next(newError(error, 500));
     }
   };
 };
 
-// Erase the record (DELETE)
-Marina.Erase = mysqlConnection => {
+ElectronicWallet.PostElectronicWalletMovement = (
+  newError,
+  Query,
+  mysqlConnection
+) => {
   return (req, res, next) => {
     try {
-      mysqlConnection.query(
-        "CALL SP_DELETE_ELECTRONIC_WALLET (?);",
-        [req.body.electronic_wallet_id],
-        (err, rows, fields) => {
-          if (err) next(newError(err, 400));
-          res.status(200).send({ status: "QUOTATION DELETED" });
-        }
-      );
+      const electronicWalletMovement = req.body.electronicWalletMovement;
+
+      if (isNaN(req.params.id))
+        next(newError("el param 'id' no es un string válido.", 406));
+      else {
+        Query(mysqlConnection, "CALL SP_ElectronicWallet_GetWallet(?);", [
+          req.params.id
+        ])
+          .then(result => {
+            /* Obtiene el monto actual */
+            let oldAmount = result[0][0][0]["marinaAmount"];
+
+            /* Verifica que no se intente insertar un monto igual al existente */
+            if (oldAmount == electronicWalletMovement.newAmount)
+              next(
+                newError("El nuevo monto no puede ser igual al anterior.", 406)
+              );
+            else {
+              /* Actualiza el total del monedero e inserta un row en el historial */
+              let Promises = [
+                Query(
+                  mysqlConnection,
+                  "CALL SP_ElectronicWallet_PatchMarinaAmount(?,?);",
+                  [req.params.id, electronicWalletMovement.newAmount]
+                ),
+                Query(
+                  mysqlConnection,
+                  "CALL SP_ElectronicWalletHistoric_PostHistoric(?,?,?,?,?);",
+                  [
+                    req.params.id,
+                    oldAmount,
+                    electronicWalletMovement.newAmount,
+                    electronicWalletMovement.description,
+                    electronicWalletMovement.alterResponsable
+                  ]
+                )
+              ];
+
+              Promise.all(Promises)
+                .then(() => res.status(204).send())
+                .catch(error => next(newError(error, 400)));
+            }
+          })
+          .catch(error => next(newError(error, 400)));
+      }
     } catch (error) {
-      console.log(error);
       next(newError(error, 500));
     }
   };
 };
 
-// Update the state
-Marina.Delete = mysqlConnection => {
-  return (req, res, next) => {
-    if (!req.body.id || req.body.delete === null)
-      res.status(400).send({ error: "Undefined Object" });
-    try {
-      mysqlConnection.query(
-        "CALL SP_LOGICAL_DELETED_ELECTRONIC_WALLET (?,?);",
-        [req.body.electronic_wallet_id, req.body.logical_deleted],
-        (err, rows, fields) => {
-          if (err) next(newError(err, 400));
-          res.status(200).send({ status: "Success" });
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      next(newError(error, 500));
-    }
-  };
-};
-
-// Create a new record
-Marina.Create = mysqlConnection => {
-  return (req, res, next) => {
-    try {
-      var ld = 0;
-      mysqlConnection.query(
-        "CALL SP_CREATE_ELECTRONIC_WALLET (?,?)",
-        [req.body.marina_amount, ld],
-        (err, rows, fields) => {
-          if (err) next(newError(err, 400));
-          res.status(200).send({ status: "Success" });
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      next(newError(error, 500));
-    }
-  };
-};
-
-// Update a record
-Marina.Update = mysqlConnection => {
-  return (req, res, next) => {
-    try {
-      mysqlConnection.query(
-        "CALL SP_UPDATE_ELECTRONIC_WALLET (?,?,?)",
-        [
-          req.body.electronic_wallet_id,
-          req.body.marina_amount,
-          req.body.logical_deleted
-        ],
-        (err, rows, fields) => {
-          if (err) next(newError(err, 400));
-          res.status(200).send({ status: "QUOTATION UPDATED" });
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      next(newError(error, 500));
-    }
-  };
-};
-
-module.exports = Marina;
+module.exports = ElectronicWallet;
