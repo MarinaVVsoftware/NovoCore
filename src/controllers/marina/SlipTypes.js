d7c9588c2521879bc78acb7df886eb8af8cdfa2b
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-15 11:35:58 -0500

    Update SlipTypes.js

--- src/controllers/marina/SlipTypes.js
+++ src/controllers/marina/SlipTypes.js
@@ -6,16 +6,9 @@ SlipTypes.GetSlipTypes = (newError, Query, mysqlConnection) => {
   return (req, res, next) => {
     try {
       Query(mysqlConnection, "CALL SP_SlipTypes_GetSlipTypes();")
-        .then(result => {
-          res.status(200).send({ slipTypes: result[0][0] });
-        })
-        .catch(error => {
-          /* retorna el mensaje de error */
-          console.log(error);
-          next(error);
-        });
+        .then(result => res.status(200).send({ slipTypes: result[0][0] }))
+        .catch(error => next(newError(error, 400)));
     } catch (error) {
-      console.log(error);
       next(newError(error, 500));
     }
   };
